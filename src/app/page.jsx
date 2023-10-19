d610a842ff7074934880436b2041e5fb97d32ab1
Yash Gorasia <y.t.gorasia@gmail.com>
2023-10-19 16:55:11 +0530

    Mobile layout created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,10 @@ import illustrationImgDesktop from "../../public/illustration-sign-up-desktop.sv
 export default function Page() {
   return (
     <main>
-      <div className="image">
-        <Image src={illustrationImg} />
-        {/* <Image src={illustrationImgDesktop} /> */}
-      </div>
+      {/* <div className="image"> */}
+      <Image src={illustrationImg} alt="Newsletter Image" className="illustration" />
+      {/* <Image src={illustrationImgDesktop} /> */}
+      {/* </div> */}
       <div className="content">
         <h1>Stay updated!</h1>
         <p>Join 60,000+ product managers receiving monthly updates on:</p>
@@ -18,12 +18,22 @@ export default function Page() {
           <li className="benefits__item">And much more!</li>
         </ul>
         <div className="email-input">
-          <label htmlFor="email">Email address</label>
-          <input type="email" name="email" id="email" />
+          <div className="label-wrapper">
+            <label htmlFor="email">Email address</label>
+            <em className="error" aria-hidden="true">
+              Valid email required
+            </em>
+          </div>
+          <input type="email" name="email" id="email" placeholder="email@company.com" />
         </div>
-        <button type="submit">Subscribe to monthly newsletter</button>
+        <button className="btn" type="submit">
+          Subscribe to monthly newsletter
+        </button>
       </div>
     </main>
+
+    // 😁 I actually stashed it and not I don't know if I will use it or not. SO I will keep it till the project is completed.
+
     // <div className="container">
     //   <div className="image-container"></div>
     //   <div className="text-container">
